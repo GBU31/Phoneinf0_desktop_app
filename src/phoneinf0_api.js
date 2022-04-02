bebef83168b410422348afb8befae47b94ba9319
brookehorizon <86805843+brookehorizon@users.noreply.github.com>
2022-04-02 19:47:27 +0300

    Update phoneinf0_api.js

--- src/phoneinf0_api.js
+++ src/phoneinf0_api.js
@@ -1,7 +1,5 @@
 function Run() {
-
   let data = {pn:document.getElementById("id_pn").value};
-
   fetch("https://phoneinf0.herokuapp.com/api/", {
     method: "POST",
     headers: {'Content-Type': 'application/json'}, 
