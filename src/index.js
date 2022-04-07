8aabd02f63f110eaf15ca779b0764d143f17f34e
brookehorizon <danieltashman17@gmail.com>
2022-04-07 09:45:37 +0300

    Updated

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  mainWindow.autoHideMenuBar();
+  
   
 };
 
--- src/phoneinf0_api.js
+++ src/phoneinf0_api.js
@@ -1,6 +1,6 @@
 function Run() {
   let data = {pn:document.getElementById("id_pn").value};
-  fetch("https://phoneinf0.herokuapp.com/api/", {
+  fetch('https://phoneinf0.herokuapp.com/api/', {
     method: "POST",
     headers: {'Content-Type': 'application/json'}, 
     body: JSON.parse(JSON.stringify(data)),
