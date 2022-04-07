30d400bd30ecdc222e94d8b90a87f23a638a3969
brookehorizon <danieltashman17@gmail.com>
2022-04-07 09:59:48 +0300

    Updated

--- src/phoneinf0_api.js
+++ src/phoneinf0_api.js
@@ -1,10 +1,16 @@
 function Run() {
-  let data = {pn:document.getElementById("id_pn").value};
-  fetch('https://phoneinf0.herokuapp.com/api/', {
-    method: "POST",
-    headers: {'Content-Type': 'application/json'}, 
-    body: JSON.parse(JSON.stringify(data)),
-  }).then(res => {
-    alert(res)
-  });
+  async function postData(url, data = {}) {
+    const response = await fetch(url, {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: data
+      
+    });
+    return response.json();
+  }
+  postData('https://phoneinf0.herokuapp.com/api/', 
+  "{ 'pn': '+962778910319' }")
+    .then(data => {
+      alert(data);
+    });
 }
